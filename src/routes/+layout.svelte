4d45c101442a6e4619ff3fc36ad80b13ffc324b4
Nihar <nihar.nandan@gmail.com>
2025-05-04 11:44:20 +0530

    prevent zoom

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+</svelte:head>
+
 {@render children()}
